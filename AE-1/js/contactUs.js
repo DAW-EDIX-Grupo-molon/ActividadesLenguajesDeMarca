d7dc55b779ab87920b4de97431e411d399fa0b30
Elogin <86604232+elogin91@users.noreply.github.com>
2023-04-05 13:35:01 +0200

    adding js

---
+++ AE-1/js/contactUs.js
@@ -0,0 +1,7 @@
+function seleccionandoFormulario(a)
+{
+    if(a==1)
+        document.getElementById("enterpreiserForm").style.display="block";
+    else if (a==2)
+        document.getElementById("developForm").style.display="block";
+}
