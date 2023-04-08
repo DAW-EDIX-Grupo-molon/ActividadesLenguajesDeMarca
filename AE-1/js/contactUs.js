1fd8c9524a2b480cc86bf23c23e61221ed472527
Elogin <86604232+elogin91@users.noreply.github.com>
2023-04-08 14:03:28 +0200

    adding footer

--- AE-1/js/contactUs.js
+++
@@ -1,7 +0,0 @@
-function seleccionandoFormulario(a)
-{
-    if(a==1)
-        document.getElementById("enterpreiserForm").style.display="block";
-    else if (a==2)
-        document.getElementById("developForm").style.display="block";
-}
